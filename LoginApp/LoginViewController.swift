efd712b5eebd65a501e930e7a044af062a48ab08
Albert Razhapov <razhapov_albert@icloud.com>
2021-02-03 01:38:24 +0300

    the foundation is laid

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by PenguinRaja on 03.02.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+
+}
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by PenguinRaja on 03.02.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+//     MARK: - Navigation
+
+     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+
+}
